298d0d2d409ac27c2d2f24bae4bd99874afab103
Maksim Komkov <KomkovMS@yandex.ru>
2023-04-16 11:04:59 +0700

    Java Hw1 Task2 (ок)

--- src/main/java/Hw1/Hw1Task2.java
+++ src/main/java/Hw1/Hw1Task2.java
@@ -1,10 +1,11 @@
 package Hw1;
+
 // Вывести все простые числа от 1 до 1000
 public class Hw1Task2 {
     public static void main(String[] args) {
         for (int i = 2; i < 1001; i++) {
             boolean flag = true;
-            for (int j = 2; j*j <= i; j++) {
+            for (int j = 2; j * j <= i; j++) {
                 if (i % j == 0) {
                     flag = false;
                     break;
