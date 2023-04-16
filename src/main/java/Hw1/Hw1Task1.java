8c30b1579d62ffffde43d736ea7f46d716df30f0
Maksim Komkov <KomkovMS@yandex.ru>
2023-04-16 10:50:53 +0700

    Java Hw1 Task1 (ок)

--- src/main/java/Hw1/Hw1Task1.java
+++ src/main/java/Hw1/Hw1Task1.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 чисел от 1 до n)
  */
 public class Hw1Task1 {
-    static int getSum(int n) {
+    /**
+     * Вычисляет сумму чисел от 1 до n.
+     * @param n число, до которого нужно посчитать сумму.
+     * @return сумма чисел от 1 до n.
+     */
+    static int calculateSum(int n) {
         int result = 0;
         for (int i = 0; i <= n; i++) {
             result += i;
@@ -13,7 +18,12 @@ public class Hw1Task1 {
         return result;
     }
 
-    static int getMult(int n) {
+    /**
+     * Вычисляет факториал числа n.
+     * @param n число, факториал которого нужно посчитать.
+     * @return факториал числа n.
+     */
+    static int calculateFactorial(int n) {
         int result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
@@ -23,12 +33,17 @@ public class Hw1Task1 {
 
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
-        System.out.printf("Enter number n = ");
+        System.out.print("Enter number n = ");
+        if (!iScanner.hasNextInt()) {
+            System.out.println("Ошибка: введено не число.");
+            System.exit(0);
+        }
         int n = iScanner.nextInt();
-        int totalSum = getSum(n);
-        int totalMult = getMult(n);
+        int totalSum = calculateSum(n);
+        long totalMult = calculateFactorial(n);
         System.out.printf("Sum of numbers from 1 to n = %d\n", totalSum);
         System.out.printf("Product of numbers from 1 to n = %d", totalMult);
-        // n = 4 1+2+3+4 = 10 1*2*3*4 = 24
+
+        iScanner.close();
     }
 }
