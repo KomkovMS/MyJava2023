6ba9cb63ae0c027db64f89e697cb6a3e39708f4d
Maksim Komkov <KomkovMS@yandex.ru>
2023-04-16 17:13:10 +0700

    GB Sem1 Task1. Написать программу, которая запросит пользователя ввести <Имя> в консоли. Получит введенную строку и выведет в консоль сообщение “Привет, <Имя>!”

---
+++ src/main/java/Seminar1/Task1.java
@@ -0,0 +1,15 @@
+package Seminar1;
+import java.util.Scanner;
+/*
+Написать программу, которая запросит пользователя ввести <Имя> в консоли.
+Получит введенную строку и выведет в консоль сообщение “Привет, <Имя>!”
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Hello what is your name?: ");
+        String name = iScanner.nextLine();
+        System.out.printf("Привет, %s!", name);
+        iScanner.close();
+    }
+}
