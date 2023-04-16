61e567bc1f066018e4acf7e0d12c7e4094a9f4ea
Maksim Komkov <KomkovMS@yandex.ru>
2023-04-16 12:30:20 +0700

    Java Hw1 Task4 (нет решения)

---
+++ src/main/java/Hw1/Hw1Task4.java
@@ -0,0 +1,28 @@
+package Hw1;
+
+import java.util.Scanner;
+
+/*
+ * Дополнительно: +Задано уравнение вида q + w = e, q, w, e >= 0.
+ * Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69.
+ * Требуется восстановить выражение до верного равенства.
+ * Предложить хотя бы одно решение или сообщить, что его нет.
+ * Под знаками вопросов - одна и та же цифра
+ * Ввод: 2? + ?5 = 69 Вывод: 24 + 45 = 69
+ */
+//public class Hw1Task4 {
+//    public static void main(String[] args) {
+//        Scanner iScanner = new Scanner(System.in);
+//        System.out.print("Введите уравнение вида q? + w? = e: ");
+//        String expression = iScanner.nextLine();
+//        String q = "", w = "", e = "";
+//        for (int i = 0; i < expression.length(); i++) {
+//            if (expression.charAt(i) == '?') {
+//                for (int j = 0; j < 10; j++) {
+//                    pass
+//                }
+//            }
+//        }
+//    }
+//
+//}
