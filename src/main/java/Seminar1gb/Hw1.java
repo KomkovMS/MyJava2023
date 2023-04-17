6b1108bd89dbd8f6a2120ebde34a5cf230ed8398
Maksim Komkov <KomkovMS@yandex.ru>
2023-04-17 20:07:22 +0700

    GB Hw1 (другой поток в записи)

---
+++ src/main/java/Seminar1gb/Hw1.java
@@ -0,0 +1,15 @@
+package Seminar1gb;
+
+import java.util.Random;
+
+/*
+Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
+ */
+public class Hw1 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int i = r.nextInt(0, 2000);
+        System.out.println(i);
+
+    }
+}
