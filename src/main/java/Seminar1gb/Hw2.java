759d4726d207bb7ae06f0945ede5f72f171f357f
Maksim Komkov <KomkovMS@yandex.ru>
2023-04-17 20:21:10 +0700

    GB Hw2 (другой поток в записи)

---
+++ src/main/java/Seminar1gb/Hw2.java
@@ -0,0 +1,20 @@
+package Seminar1gb;
+import java.util.Random;
+/*
+Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+ */
+public class Hw2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int x = random.nextInt(); // Получаем случайное число
+        int n = 0;
+        while (x != 0) {
+            n++;
+            x >>>= 1;
+        }
+        System.out.println("Случайное число: " + x);
+        System.out.println("Номер старшего значащего бита: " + n);
+
+
+    }
+}
