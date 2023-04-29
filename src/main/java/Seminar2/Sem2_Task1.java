9579fd65a2b926ca4425d9ee1a60e6457ce40b1b
Maksim Komkov <KomkovMS@yandex.ru>
2023-04-29 17:55:44 +0700

    GB Sem2 Task1 Дано четное число N (>0) и символы c1 и c2. Написать метод, который вернет строку длины N, которая состоит из чередующихся символов c1 и c2, начиная с c1. 6 a b ababab + обернул в метод

--- src/main/java/Seminar2/Sem2_Task1.java
+++ src/main/java/Seminar2/Sem2_Task1.java
@@ -9,20 +9,23 @@ a b
 ababab
  */
 public class Sem2_Task1 {
+    // мое решение
+    static String myString(String c1, String c2, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n / 2; i++) {
+            result.append(c1 + c2);
+        }
+        return result.toString();
+    }
+
     public static void main(String[] args) {
-        // мое решение
+
         int n = 6;
         String c1 = "a";
         String c2 = "b";
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < n/2; i++) {
-            result.append(c1+c2);
-        }
 
-        System.out.println(result.toString());
-
-
-
+        String result = myString(c1, c2, n);
+        System.out.println(result);
 
 
     }
