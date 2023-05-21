87ecd984e832f9ff3757955bffdc5e789bcdea0b
Maksim Komkov <KomkovMS@yandex.ru>
2023-05-21 14:28:46 +0700

    GB Java Урок 6. Хранение и обработка данных ч3: множество коллекций Set

---
+++ src/main/java/Lesson6/part2/Worker.java
@@ -0,0 +1,23 @@
+package Lesson6.part2;
+
+public class Worker {
+    int id;
+    int salary; // зарплата
+    String firstName;
+    String lastName;
+
+    @Override
+    public String toString() {
+        return String.format("%d %s %s %d", id, firstName, lastName, salary);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        var t = (Worker) o;
+        return id == t.id && firstName == t.firstName;
+    }
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
