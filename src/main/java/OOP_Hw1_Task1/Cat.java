2ba7038fcc5f31954b8c9c1b219b652143377753
Maksim Komkov <KomkovMS@yandex.ru>
2023-05-31 22:34:02 +0700

    ООП. Урок 1. Принципы ООП: Инкапсуляция, наследование, полиморфизм. Задача 1: Создайте класс "Кот" (Cat) со следующими свойствами:
    Приватное поле "имя" (name) типа String для хранения имени кота.
    Приватное поле "возраст" (age) типа int для хранения возраста кота.
    Публичные методы доступа (геттеры и сеттеры) для полей "имя" и "возраст".
    Публичный метод "приветствие" (greet), который выводит на консоль приветствие
    вида "Мяу! Меня зовут <имя>. Мне <возраст> года(лет).".
    
    Дополнительное задание:
    Создайте класс "Владелец" (Owner) со свойством "имя" (name) типа String.
    Добавьте соответствующее поле в классе "Кот" и методы доступа для него. Измените
    метод "приветствие" класса "Кот", чтобы он выводил приветствие вида "Мяу! Меня
    зовут <имя>. Мне <возраст> года(лет). Мой владелец - <имя владельца>.".

---
+++ src/main/java/OOP_Hw1_Task1/Cat.java
@@ -0,0 +1,89 @@
+package OOP_Hw1_Task1;
+
+/**
+ * Класс, представляющий кота.
+ */
+public class Cat {
+    private String name;
+    private int age;
+    private Owner owner;
+
+    /**
+     * Конструктор класса.
+     *
+     * @param name Имя кота.
+     * @param age Возраст кота.
+     */
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * Получает имя кота.
+     *
+     * @return Имя кота.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Устанавливает имя кота.
+     *
+     * @param name Имя кота.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Получает возраст кота.
+     *
+     * @return Возраст кота.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Устанавливает возраст кота.
+     *
+     * @param age Возраст кота.
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * Получает владельца кота.
+     *
+     * @return Владелец кота.
+     */
+    public Owner getOwner() {
+        return owner;
+    }
+
+    /**
+     * Устанавливает владельца кота.
+     *
+     * @param owner Владелец кота.
+     */
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    /**
+     * Выводит приветствие кота в консоль.
+     */
+    public void greet() {
+        String ownerName;
+        if (owner != null) {
+            ownerName = owner.getName();
+        } else {
+            ownerName = "неизвестный владелец";
+        }
+        System.out.println("Мяу! Меня зовут " + name + ". Мне " + age +
+                " года(лет). Мой владелец - " + ownerName + ".");
+    }
+}
---
+++ src/main/java/OOP_Hw1_Task1/Owner.java
@@ -0,0 +1,35 @@
+package OOP_Hw1_Task1;
+
+/**
+ * Класс, представляющий владельца кота.
+ */
+public class Owner {
+    private String name;
+
+    /**
+     * Конструктор класса.
+     *
+     * @param name Имя владельца.
+     */
+    public Owner(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Получает имя владельца.
+     *
+     * @return Имя владельца.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Устанавливает имя владельца.
+     *
+     * @param name Имя владельца.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
