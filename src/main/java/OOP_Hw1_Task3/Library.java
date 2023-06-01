598aa84f37b44af3f28ad00ae00d62d3bcbb2b97
Maksim Komkov <KomkovMS@yandex.ru>
2023-06-01 22:25:05 +0700

    ООП. Урок 1. Принципы ООП: Инкапсуляция, наследование, полиморфизм. Задача 3: Реализуйте систему управления библиотекой. Создайте классы "Книга" (Book) и "Библиотека" (Library) со следующими свойствами и методами:
    Класс "Книга" (Book):
    
    Приватные поля "название" (title) и "автор" (author) типа String для хранения
    названия и автора книги соответственно.
    Приватное поле "доступность" (available) типа boolean для указания доступности
    книги.
    Публичные методы доступа (геттеры и сеттеры) для полей "название" и "автор".
    Публичные методы доступа (геттеры и сеттеры) для поля "доступность".
    Публичный метод "показать информацию" (displayInfo), который выводит на консоль
    информацию о книге (название, автор, доступность).
    
    Класс "Библиотека" (Library):
    
    Приватное поле "каталог" (catalog) типа ArrayList<Book> для хранения списка книг
    в библиотеке.
    Публичный метод "добавить книгу" (addBook), который принимает объект типа Book и
    добавляет его в каталог библиотеки.
    Публичный метод "удалить книгу" (removeBook), который принимает объект типа Book
    и удаляет его из каталога библиотеки.
    Публичный метод "показать доступные книги" (displayAvailableBooks), который
    выводит на консоль информацию о доступных книгах в библиотеке.
    Публичный метод "поиск книги по автору" (searchByAuthor), который принимает имя
    автора в качестве параметра и выводит на консоль информацию о книгах данного
    автора, находящихся в каталоге библиотеки.

---
+++ src/main/java/OOP_Hw1_Task3/Library.java
@@ -0,0 +1,62 @@
+package OOP_Hw1_Task3;
+
+import java.util.ArrayList;
+
+/**
+ * Класс, представляющий библиотеку.
+ */
+class Library {
+    private ArrayList<Book> catalog; // Каталог книг
+
+    /**
+     * Конструктор класса Library.
+     */
+    public Library() {
+        catalog = new ArrayList<>();
+    }
+
+    /**
+     * Метод для добавления книги в каталог библиотеки.
+     *
+     * @param book объект типа Book для добавления
+     */
+    public void addBook(Book book) {
+        catalog.add(book);
+    }
+
+    /**
+     * Метод для удаления книги из каталога библиотеки.
+     *
+     * @param book объект типа Book для удаления
+     */
+    public void removeBook(Book book) {
+        catalog.remove(book);
+    }
+
+    /**
+     * Метод для отображения информации о доступных книгах в библиотеке.
+     */
+    public void displayAvailableBooks() {
+        System.out.println("Доступные книги:");
+        for (Book book : catalog) {
+            if (book.isAvailable()) {
+                book.displayInfo();
+                System.out.println();
+            }
+        }
+    }
+
+    /** Метод для поиска книги по имени автора.
+     *
+     * @param author имя автора
+     */
+    public void searchByAuthor(String author) {
+        System.out.println("Книги автора " + author + ":");
+        for (Book book : catalog) {
+            if (book.getAuthor().equals(author)) {
+                book.displayInfo();
+                System.out.println();
+            }
+        }
+    }
+}
